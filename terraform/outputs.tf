83bbcdbabc465e298e1918620b24193b0573c046
wenqiglantz <wenqiglantz@gmail.com>
2023-12-31 17:39:33 -0500

    refactoring

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,35 +1,7 @@
 output "ecs_cluster" {
-  value = module.fargate.ecs_cluster
+  value = module.ecs.cluster_name
 }
 
-output "ecs_service" {
-  value = module.fargate.ecs_service
-}
-
-output "ecs_task_definition" {
-  value = module.fargate.ecs_task_definition
-}
-
-output "container_name" {
-  value = module.fargate.container_name
-}
-
-output "ecr_repository_name" {
-  value = module.fargate.ecr_repository_name
-}
-
-output "app_test" {
-  value = module.fargate.app_test
-}
-
-output "ecs_alb_target_group_arn" {
-  value = module.cluster_alb.ecs_alb_target_group_arn
-}
-
-output "alb_security_group_id" {
-  value = module.cluster_alb.alb_security_group_id
-}
-
-output "fargate_task_security_group_id" {
-  value = module.fargate.fargate_task_security_group_id
+output "services" {
+  value = module.ecs.services
 }
