1e6ccb9d9ae47b032e860216c778131977efce25
wenqiglantz <wenqiglantz@gmail.com>
2024-01-05 17:15:20 -0500

    polishing

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,21 +2,12 @@ output "ecs_cluster" {
   value = module.ecs.cluster_name
 }
 
-output "services" {
-  value = module.ecs.services
-}
-
-output "repository_arn" {
-  description = "Full ARN of the repository"
-  value       = module.ecr.repository_arn
-}
-
-output "repository_registry_id" {
-  description = "The registry ID where the repository was created"
-  value       = module.ecr.repository_registry_id
-}
-
 output "repository_url" {
   description = "The URL of the repository"
   value       = module.ecr.repository_url
+}
+
+output "alb_dns" {
+  description = "The DNS name of ALB"
+  value       = module.alb.dns_name
 }
